3c2b92844336983249df77f3d12c87d9442c9c77
Tan Tano <tantn1@sunshinegroup.vn>
2020-06-05 10:07:04 +0700

    Chương 3 bài 2 Hướng dẫn tạo model, view, controller

---
+++ WebsiteBanHang/WebsiteBanHang/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection f)
+        {
+            Student st = new Student();
+            st.Id = int.Parse(f[0]);
+            st.Name = f.Get("Name");
+            return View(st);
+        }
+    }
+}
---
+++ WebsiteBanHang/WebsiteBanHang/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteBanHang.Models
+{
+    public class Student
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
